f2dacf7a7a65101164f1ae03d063cb90295a2ba3
Takeshi Yonezu <yonezu@soramitsu.co.jp>
2016-11-21 16:01:07 +0900

    Change container start command to bash /start_iroha.sh.

--- iroha/iroha3.sh
+++ iroha/iroha3.sh
@@ -10,4 +10,4 @@ docker rm ${IROHA}
 
 docker run -it --name ${IROHA} --shm-size 1g \
   -v ${HOME}/config${IROHA_NO}:/opt/iroha/config \
-  soramitsu/iroha /bin/bash
+  soramitsu/iroha /bin/bash /start_iroha.sh
