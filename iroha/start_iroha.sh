39adc67aeae0a02c9cd63d6df4ad99f30cc3e109
Takeshi Yonezu <yonezu@yonezu.local>
2016-11-21 15:30:15 +0900

    Dockerfile CMD ["/bin/bash","/start_iroha.sh"] for Automatically start.

--- iroha/start_iroha.sh
+++ iroha/start_iroha.sh
@@ -3,4 +3,6 @@
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 export IROHA_HOME=/opt/iroha
 
-java -cp ${IROHA_HOME}/core/vendor/Aeron/aeron-driver/build/libs/aeron-driver-1.0.5-SNAPSHOT-all.jar io.aeron.driver.MediaDriver &
+su - iroha -c "java -cp ${IROHA_HOME}/core/vendor/Aeron/aeron-driver/build/libs/aeron-driver-1.0.5-SNAPSHOT-all.jar io.aeron.driver.MediaDriver &"
+
+exec /bin/bash
