4c9b526c61a7f47ca530e19a3247dc3201084b8b
Takeshi Yonezu <yonezu@soramitsu.co.jp>
2016-11-22 09:43:11 +0900

    Remove unused shells.

--- iroha/a2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker stop iroha2
-docker rm iroha2
-
-docker run -it --name iroha2 --shm-size 1g \
-  -v /Users/yonezu/git/soramitsu/Dockerfiles.git/iroha/config2:/opt/iroha/config \
-  soramitsu/iroha /bin/bash
--- iroha/a3.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker stop iroha3
-docker rm iroha3
-
-docker run -it --name iroha3 --shm-size 1g \
-  -v /Users/yonezu/git/soramitsu/Dockerfiles.git/iroha/config3:/opt/iroha/config \
-  soramitsu/iroha /bin/bash
