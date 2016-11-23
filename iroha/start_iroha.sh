a298db93a806cba67b71c73071c474be54310a7a
Takeshi Yonezu <yonezu@soramitsu.co.jp>
2016-11-23 18:55:37 +0900

    Remove verbose echo statements.

--- iroha/start_iroha.sh
+++ iroha/start_iroha.sh
@@ -5,8 +5,4 @@ export IROHA_HOME=/usr/local/iroha
 
 su - iroha -c "java -cp ${IROHA_HOME}/libs/aeron-driver-1.0.5-SNAPSHOT-all.jar io.aeron.driver.MediaDriver &"
 
-echo ">>> You can execute IROHA by iroha user."
-echo ">>> So, you should type \"su - iroha\" at first."
-echo ">>> Have a fantastic IROHA world!."
-
 exec /bin/bash
