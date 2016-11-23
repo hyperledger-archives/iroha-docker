13186e77ebb4fbb8775be261b72ac73ddbb33f9d
Takeshi Yonezu <yonezu@soramitsu.co.jp>
2016-11-23 18:54:45 +0900

    Reduce shm-size to 512MB.

--- iroha/iroha1.sh
+++ iroha/iroha1.sh
@@ -8,7 +8,7 @@ HOME=$(pwd)
 docker stop ${IROHA}
 docker rm ${IROHA}
 
-docker run -it --name ${IROHA} --shm-size 1g \
+docker run -it --name ${IROHA} --shm-size 512m \
   -v ${HOME}/config${IROHA_NO}:/usr/local/iroha/config \
   -v /var/tmp:/var/tmp \
   soramitsu/iroha /bin/bash 
--- iroha/iroha2.sh
+++ iroha/iroha2.sh
@@ -8,6 +8,6 @@ HOME=$(pwd)
 docker stop ${IROHA}
 docker rm ${IROHA}
 
-docker run -it --name ${IROHA} --shm-size 1g \
+docker run -it --name ${IROHA} --shm-size 512m \
   -v ${HOME}/config${IROHA_NO}:/usr/local/iroha/config \
   soramitsu/iroha /bin/bash
