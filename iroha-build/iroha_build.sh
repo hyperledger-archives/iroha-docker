98e66dfbb2407ed7f7555289938faa5804cc0bce
Takeshi Yonezu <yonezu@soramitsu.co.jp>
2016-12-05 15:35:10 +0900

    Change for remove start_iroha.sh and add sudo command for apt install.

--- iroha-build/iroha_build.sh
+++ iroha-build/iroha_build.sh
@@ -23,7 +23,7 @@ git clone https://github.com/google/leveldb.git core/vendor/leveldb
 git clone https://github.com/MizukiSonoko/ed25519.git core/vendor/ed25519
 git clone --recursive https://github.com/MizukiSonoko/Cappuccino.git core/vendor/Cappuccino
 
-apt -y install autoconf automake libtool pkg-config
+sudo apt -y install autoconf automake libtool pkg-config
 
 cd /tmp
 git clone -b v3.0.0 https://github.com/google/protobuf.git
@@ -33,7 +33,7 @@ git cherry-pick 1760feb621a913189b90fe8595fffb74bce84598
 ./autogen.sh
 ./configure --prefix=/usr
 make -j 14
-make install
+sudo make install
 
 cd /tmp
 git clone -b $(curl -L http://grpc.io/release) https://github.com/grpc/grpc
@@ -41,7 +41,7 @@ git clone -b $(curl -L http://grpc.io/release) https://github.com/grpc/grpc
 cd grpc
 git submodule update --init
 make
-make install
+sudo make install
 
 cd ${IROHA_HOME}/core/vendor/leveldb
 make
@@ -61,25 +61,8 @@ cmake ..
 make
 
 sudo mkdir ${IROHA_DEST}
-cd ${IROHA_HOME}
-sudo rsync -av config core/vendor/Aeron/aeron-driver/build/libs ${IROHA_DEST}
 
-cd build
+cd ${IROHA_HOME}/build
 sudo rsync -av bin lib my_test_bin test_bin ${IROHA_DEST}
-
-if [ -f /usr/local/bin/start_iroha.sh ]; then
-  sudo mv /usr/local/bin/start_iroha.sh ${IROHA_DEST}
-else
-  sudo rsync -av ./start_iroha.sh ${IROHA_DEST}
-fi
-
-if [ -f /usr/local/bin/test.sh ]; then
-  sudo mv /usr/local/bin/test.sh ${IROHA_DEST}
-else
-  sudo rsync -av ./test.sh ${IROHA_DEST}
-fi
-
-sudo chown iroha:iroha ${IROHA_DEST}/start_iroha.sh ${IROHA_DEST}/test.sh
-sudo chmod 0755 ${IROHA_DEST}/test.sh
 
 exit 0
