241d24326c675c81eb8a3aec055d7a43e77f80dc
Takeshi Yonezu <yonezu@soramitsu.co.jp>
2016-12-06 10:55:06 +0900

    Add echo commads to show script execution.

--- iroha/run-iroha.sh
+++ iroha/run-iroha.sh
@@ -12,8 +12,13 @@ for i in 1 2 3 4; do
   IROHA_NO="$i"
   IROHA="iroha${IROHA_NO}"
 
+  echo "# docker stop ${IROHA}"
   docker stop ${IROHA}
+
+  echo "# docker rm ${IROHA}"
   docker rm ${IROHA}
+
+  echo "# docker run -d --name ${IROHA} -v ${HOME}/config${IROHA_NO}:/usr/local/iroha/config hyperledger/${IROHA_NAME}"
 
   docker run -d --name ${IROHA} \
     -v ${HOME}/config${IROHA_NO}:/usr/local/iroha/config \
--- iroha/run-sumeragi.sh
+++ iroha/run-sumeragi.sh
@@ -5,8 +5,13 @@ HOME=$(pwd)
 IROHA_NO="3"
 IROHA="iroha${IROHA_NO}"
 
+echo "# docker stop ${IROHA}"
 docker stop ${IROHA}
+
+echo "# docker rm ${IROHA}"
 docker rm ${IROHA}
+
+echo "# docker run -it --name ${IROHA} -v ${HOME}/config${IROHA_NO}:/usr/local/iroha/config hyperledger/iroha /bin/bash"
 
 docker run -it --name ${IROHA} \
   -v ${HOME}/config${IROHA_NO}:/usr/local/iroha/config \
